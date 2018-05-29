92bdbead5cc515c666a258caababd1afdb725d30
Samuel Cooper <cooper.samuel@outlook.com>
2018-05-29 11:50:04 -0400

    adding some client stuff

---
+++ client/components/songCreate.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
+
+class SongCreate extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: ''
+        }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.mutate({
+            variables: { title: this.state.title },
+            refetchQueries: [{ query: query }]
+        }).then(() => {
+            hashHistory.push('/'); // Route the user back to the Song List on submit
+        }).catch(error => {
+            // Throw errors here if the request fails
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Link to="/">Back</Link>
+                <h3>Create A New Song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input  onChange={event => this.setState({ title: event.target.value })}
+                            value={this.state.title} />
+                </form>
+            </div>
+        );
+    }
+}
+
+const mutation = gql`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+            id
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
